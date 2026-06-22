87c076c459f6fb9ccfd6452c427c7c47b5f40503
alexsmyshliaev <alexsmyshliaev@gmail.com>
2026-06-22 17:17:22 +0300

    обновил контакты

--- frontend/assets/js/config.js
+++ frontend/assets/js/config.js
@@ -61,11 +61,11 @@ window.ECS_CONFIG = {
      Заполните claimPhone и claimEmail проверенными данными.
      Пустые поля показываются как «указан в полисе» / «уточните на сайте». */
   insurers: [
-    { name: "Ренессанс Страхование", site: "https://www.renins.ru",  claimPhone: "+7 800 333-88-00", claimEmail: "" },
+    { name: "Ренессанс Страхование", site: "https://www.renins.ru",  claimPhone: "8 (800) 333-88-00", claimEmail: "" },
     { name: "Тинькофф Страхование",  site: "8 800 700-55-00",                       claimPhone: "", claimEmail: "" },
-    { name: "Ингосстрах",            site: "https://www.ingos.ru/incident/osago",    claimPhone: "", claimEmail: "" },
-    { name: "АльфаСтрахование",      site: "https://www.alfastrah.ru",claimPhone: "", claimEmail: "" },
-    { name: "Росгосстрах",           site: "https://www.rgs.ru",      claimPhone: "", claimEmail: "" },
+    { name: "Ингосстрах",            site: "https://www.ingos.ru/incident/osago",    claimPhone: "+7 (495) 956-55-55", claimEmail: "" },
+    { name: "АльфаСтрахование",      site: "https://www.alfastrah.ru",claimPhone: "8 (800) 333-0-999", claimEmail: "" },
+    { name: "Росгосстрах",           site: "https://www.rgs.ru",      claimPhone: " 8 (800) 200-09-00", claimEmail: "" },
     { name: "Совкомбанк Страхование",site: "",                       claimPhone: "", claimEmail: "" },
     { name: "Зетта Страхование",     site: "https://www.zettains.ru", claimPhone: "", claimEmail: "" },
     { name: "ВСК",                   site: "https://www.vsk.ru",      claimPhone: "", claimEmail: "" },
